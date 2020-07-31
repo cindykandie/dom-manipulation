e7ad05e7ca0996cb2df0d2d5ade77d48d5dc7503
Cindy Kandie <37507239+cindykandie@users.noreply.github.com>
2020-08-01 00:25:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let mainSection = document.getElementById('main')
+
+mainSection.style.backgroundColor="blue"
+mainSection.style.padding=0
+mainSection.style.margin=0
+mainSection.style.display="flex"
+
+function modalDisplayer(){
+    mainSection.style.opacity=1
+}
